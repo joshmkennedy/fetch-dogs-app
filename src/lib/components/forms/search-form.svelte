371e7e5e1d178469abb3c2a42f339c09e9b4b7e1
josh kennedy <josh@outthinkgroup.com>
2025-02-04 17:18:31 -0500

    adds breeds component

---
+++ src/lib/components/forms/search-form.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+	import Button from "../ui/button/button.svelte";
+	import BreedsSelect from "./search-form/breeds-select.svelte";
+	const data = $props()
+	let tags = $state<{label?:string; value:string;}[]>([]);
+</script>
+
+<form>
+	<BreedsSelect bind:tags breeds={data.breeds}/>
+	<Button type="submit">Find My New Buddy</Button>
+</form>
