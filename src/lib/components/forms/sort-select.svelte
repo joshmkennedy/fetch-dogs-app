feff423b13e8fa6ce1f6cc066b007eaeef795672
josh kennedy <josh@outthinkgroup.com>
2025-02-09 20:44:37 -0500

    sort direction now is an icon

--- src/lib/components/forms/sort-select.svelte
+++ src/lib/components/forms/sort-select.svelte
@@ -8,13 +8,20 @@
 	import { updateURLAndRevalidate } from '$lib/utils';
 	import type { SortCategory, SortDirection } from '$lib/types/api';
 	import DropdownMenu from '$lib/components/ui/dropdown-menu/my-dropdown-menu.svelte';
+	import { ArrowDown, ArrowUp } from 'svelte-radix';
 
 	const sortCategoryOptions = ['breed', 'name', 'age'];
-	const sortDirectionOptions = ['asc', 'desc'];
+	const sortDirectionOptions = [
+		{ value: 'asc', label: 'Ascending', renderFn: ascendingArrow },
+		{ value: 'desc', label: 'Descending', renderFn: descendingArrow }
+	];
 
 	onMount(() => {
 		if (page.url.searchParams.has('sort')) {
-			const [cat, dir] = page.url.searchParams.get('sort')?.split(':');
+			const [cat, dir] = (page.url.searchParams.get('sort')?.split(':') ?? ['breed', 'asc']) as [
+				SortCategory,
+				SortDirection
+			];
 			$SelectedCategory = cat;
 			$SelectedDirection = dir;
 		}
@@ -25,28 +32,40 @@
 	}
 </script>
 
-<div class="flex gap-0.5 rounded-lg border border-muted p-0.5">
-	<DropdownMenu
-		bind:buttonText={$SelectedCategory}
-		selectedItem={$SelectedCategory}
-		onChange={async (selected: SortCategory) => {
-			if (selected.toLowerCase() !== $SelectedCategory) {
-				$SelectedCategory = selected.toLowerCase() as SortCategory;
-				await updateSort();
-			}
-		}}
-		items={sortCategoryOptions}
-	/>
-	<span class="h-full w-[1px] bg-border"></span>
-	<DropdownMenu
-		bind:buttonText={$SelectedDirection}
-		selectedItem={$SelectedDirection}
-		onChange={async (selected: SortDirection) => {
-			if (selected.toLowerCase() !== $SelectedDirection) {
-				$SelectedDirection = selected.toLowerCase() as SortDirection;
-				await updateSort();
-			}
-		}}
-		items={sortDirectionOptions}
-	/>
+{#snippet ascendingArrow()}
+	<ArrowUp class="text-primary h-4 w-4" />
+{/snippet}
+
+{#snippet descendingArrow()}
+	<ArrowDown class="h-4 w-4 text-primary" />
+{/snippet}
+
+<div>
+	<label for="sort-by" class="text-xs text-muted-foreground">Sort By</label>
+	<div class="flex gap-0.5 rounded-lg border items-stretch border-muted p-0.5">
+		<DropdownMenu
+			bind:buttonText={$SelectedCategory}
+			selectedItem={$SelectedCategory}
+			onChange={async (selected: SortCategory) => {
+				if (selected.toLowerCase() !== $SelectedCategory) {
+					$SelectedCategory = selected.toLowerCase() as SortCategory;
+					await updateSort();
+				}
+			}}
+			items={sortCategoryOptions}
+		/>
+		<span class=" w-[2px] bg-muted block"></span>
+		<DropdownMenu
+			buttonText={$SelectedDirection === 'asc' ? ascendingArrow : descendingArrow}
+			selectedItem={$SelectedDirection}
+			onChange={async (selected: SortDirection) => {
+				if (selected.toLowerCase() !== $SelectedDirection) {
+					$SelectedDirection = selected.toLowerCase() as SortDirection;
+
+					await updateSort();
+				}
+			}}
+			items={sortDirectionOptions}
+		/>
+	</div>
 </div>
--- src/lib/components/ui/dropdown-menu/my-dropdown-menu.svelte
+++ src/lib/components/ui/dropdown-menu/my-dropdown-menu.svelte
@@ -5,12 +5,14 @@
 		DropdownMenu,
 		type WithoutChild
 	} from 'bits-ui';
+	import type { Snippet } from 'svelte';
 	import { Check } from 'svelte-radix';
 
 	type Props = DropdownMenuRootProps & {
-		buttonText: string;
-		items: string[];
+		buttonText: string | Snippet;
+		items: string[] | { label: string; value: string; renderFn?: Snippet }[];
 		selectedItem: string;
+		label?: string;
 		contentProps?: WithoutChild<DropdownMenuContentStaticProps>;
 		onChange: (selected: any) => void;
 	};
@@ -20,7 +22,8 @@
 		children,
 		buttonText = $bindable('Open'),
 		items,
-		selectedItem = $bindable(items[0]),
+		label = $bindable('Open'),
+		selectedItem = $bindable(typeof items[0] == 'object' ? items[0].value : (items[0] as string)),
 		onChange,
 		contentProps,
 		...restProps
@@ -35,25 +38,38 @@
 
 <DropdownMenu.Root bind:open {...restProps}>
 	<DropdownMenu.Trigger
-		class="flex h-full w-full items-center justify-between rounded-md px-4 py-2 text-sm text-primary transition-colors hover:bg-muted  focus:outline-none focus:ring-0"
+		class="flex w-full items-center capitalize justify-between rounded-md px-4 py-2 text-sm text-primary transition-colors hover:bg-muted  focus:outline-none focus:ring-0"
 	>
-		{buttonText}
+		{#if typeof buttonText == 'string'}
+			{buttonText}
+		{:else}
+			{@render buttonText()}
+		{/if}
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Portal>
 		<DropdownMenu.Content
 			{...contentProps}
 			class="w-full max-w-[375px] bg-background text-primary shadow-lg"
 		>
-			<DropdownMenu.RadioGroup aria-label={buttonText} bind:value={selectedItem}>
+			<DropdownMenu.RadioGroup aria-label={label} bind:value={selectedItem}>
 				{#each items as item}
 					<DropdownMenu.RadioItem
 						closeOnSelect={true}
-						value={item}
-						class="rounded-button flex h-10 w-full select-none items-center py-3 pl-5 pr-1.5 text-sm capitalize outline-none  data-[highlighted]:bg-muted"
+						value={typeof item == 'object' ? item.value : item}
+						class={`rounded-button flex h-10 w-full select-none items-center
+						py-3 pl-5 pr-1.5 text-sm capitalize outline-none
+						data-[highlighted]:bg-muted/40 data-[highlighted]:cursor-pointer ${selectedItem == item || (typeof item == 'object' && item.value == selectedItem) ? 'bg-muted' : ''}`}
 					>
-						{item}
-						{#if selectedItem == item}
-							<Check />
+							{console.log(item)}
+						{#if typeof item == 'object' }
+							{@render item.renderFn?.()}
+						{:else if typeof item == 'object' && item.label}
+							{item.label}
+						{:else}
+							{item}
+						{/if}
+						{#if selectedItem == item || (typeof item == 'object' && item.value == selectedItem)}
+							<Check class="h-4 w-4" />
 						{/if}
 					</DropdownMenu.RadioItem>
 				{/each}
